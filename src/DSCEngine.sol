25ce44229a97f648a586dd42b8de1eb55b3e64ab
Nikolay Ivanov <nick.ivanov98@gmail.com>
2024-11-30 09:03:20 +0200

    Adding simple deposit collateral

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.28;
 
 import {IDSCEngine} from "./IDSCEngine.sol";
+import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 /**
  * @title DSCEngine
@@ -22,18 +24,93 @@ import {IDSCEngine} from "./IDSCEngine.sol";
  * for minting and redeeming DSC, as well as depositing and withdrawing collateral.
  * @notice This contract is based on the MakerDAO DSS system
  */
-contract DSCEngine is IDSCEngine {
-    function depositCollateralAndMintDsc() external {}
+contract DSCEngine is IDSCEngine, ReentrancyGuard {
+    ///////////////////
+    // Errors
+    ///////////////////
+    error DSCEngine__NeedsMoreThanZeroAmount();
+    error DSCEngine__TokenAddressesAndPriceFeedsLengthDoNotMatch();
+    error DSCEngine__NotAllowedToken();
+
+    ///////////////////
+    // State Variables
+    ///////////////////
+    DecentralizedStableCoin private i_dsc;
+
+    /// @dev Mapping of token address to price feed address
+    mapping(address collateralToken => address priceFeed) private s_priceFeeds;
+    /// @dev Amount of collateral deposited by user
+    mapping(address user => mapping(address collateralToken => uint256 amount)) private s_collateralDeposited;
+
+    ///////////////////
+    // Events
+    ///////////////////
+    event CollateralDeposited(address indexed user, address indexed collateralToken, uint256 amount);
+
+    ///////////////////
+    // Modifiers
+    ///////////////////
+    modifier nonZeroAmount(uint256 _amount) {
+        if (_amount == 0) revert DSCEngine__NeedsMoreThanZeroAmount();
+        _;
+    }
+
+    modifier notUnsupportedToken(address _tokenCollateralAddress) {
+        if (s_priceFeeds[_tokenCollateralAddress] == address(0)) revert DSCEngine__NotAllowedToken();
+        _;
+    }
+
+    ///////////////////
+    // Functions
+    ///////////////////
+    constructor(address[] memory _tokenAddresses, address[] memory _priceFeedAddresses, address dscAddress) {
+        if (_tokenAddresses.length != _priceFeedAddresses.length) {
+            revert DSCEngine__TokenAddressesAndPriceFeedsLengthDoNotMatch();
+        }
+
+        for (uint256 i = 0; i < _tokenAddresses.length; i++) {
+            s_priceFeeds[_tokenAddresses[i]] = _priceFeedAddresses[i];
+        }
+
+        i_dsc = DecentralizedStableCoin(dscAddress);
+    }
+
+    ///////////////////
+    // External Functions
+    ///////////////////
+    /**
+     * @inheritdoc IDSCEngine
+     */
+    function depositCollateral(address _tokenCollateralAddress, uint256 _amountCollateral)
+        external
+        override
+        nonZeroAmount(_amountCollateral)
+        notUnsupportedToken(_tokenCollateralAddress)
+        nonReentrant
+    {
+        _depositCollateral(_tokenCollateralAddress, _amountCollateral);
+    }
+
+    function depositCollateralAndMintDsc() external override {}
 
     function redeemCollateralForDsc() external override {}
 
-    function burnDsc() external override {}
+    function redeemCollateral() external override {}
 
     function liquidate() external override {}
 
     function getHealthFactory() external view override {}
 
-    function redeemCollateral() external override {}
+    function burnDsc() external override {}
 
     function mintDsc() external override {}
+
+    ///////////////////
+    // Internal Functions
+    ///////////////////
+    function _depositCollateral(address _tokenCollateralAddress, uint256 _amountCollateral) private {
+        s_collateralDeposited[msg.sender][_tokenCollateralAddress] += _amountCollateral;
+
+        emit CollateralDeposited(msg.sender, _tokenCollateralAddress, _amountCollateral);
+    }
 }
--- src/IDSCEngine.sol
+++ src/IDSCEngine.sol
@@ -2,9 +2,14 @@
 pragma solidity ^0.8.28;
 
 interface IDSCEngine {
+    /**
+     * @param tokenCollateralAddress The address of the token to deposit as collateral
+     * @param amountCollateral The amount of tokenCollateral to deposit
+     */
+    function depositCollateral(address tokenCollateralAddress, uint256 amountCollateral) external;
+    function redeemCollateral() external;
     function depositCollateralAndMintDsc() external;
     function redeemCollateralForDsc() external;
-    function redeemCollateral() external;
     function liquidate() external;
     function getHealthFactory() external view;
     function burnDsc() external;
